1376ba6c6672f19f2d6b06f4149013ed37ff459d
zoddDev <josiesala@gmail.com>
2023-01-23 14:19:41 +0100

    Fixed neofetch and command prompt glitch

--- .config/neofetch/launch-neofetch.sh
+++ .config/neofetch/launch-neofetch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #/bin/neofetch --ascii $HOME/.config/neofetch/ascii --ascii_colors 6 --colors 4 7 7 2 2 7
-/bin/neofetch --backend kitty --source ~/.config/neofetch/image.png --size '230px' --colors 4 14 8 6 8 7 && printf ''
+/bin/neofetch --backend kitty --source ~/.config/neofetch/image.png --size '230px' --colors 4 14 8 6 8 7 ; echo && echo
